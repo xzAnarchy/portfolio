5315dda63ee708f06dd0d3c8a16c95137f4c84a1
Gabriel Mendoza <mendozagabriel726@gmail.com>
2024-06-20 16:16:02 -0400

    fix

--- my-portfolio/src/app/page.js
+++ my-portfolio/src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div className='bg-[#25325F] text-slate-200'>
       <Navbar />
       <div className='lg:mx-[200px]'>
-        <div className='sm:mt-[150px] sm:mb-[100px] mt-[50px] mb-[50px]'>
+        <div className='sm:mt-[150px] sm:mb-[350px] mt-[50px] mb-[50px]'>
           <Header />
         </div>
         <About />
